d92428d909ea7a33d98ae0fa0ef5b924be1394f8
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-06-20 23:00:30 +0530

    Constructor Function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (firstName, birthYear, cityName) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.cityName = cityName;
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+  this.userData = function () {
+    console.log(
+      `My name is ${this.firstName} and by age is ${calcAge()} and i live in ${
+        this.cityName
+      }`
+    );
+  };
+};
+
+const jonas = new Person('Jonas', 1991, 'Faridabad');
+console.log(jonas);
+
+const devender = new Person('Devender', 1996, 'Delhi');
+console.log(devender);
+const jay = 'Jay';
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
